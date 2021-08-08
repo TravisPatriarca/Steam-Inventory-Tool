d9493378192c4df9902a0af58366765bce33ef70
Travis Patriarca <trav.patriarca@gmail.com>
2021-08-08 19:19:26 +0800

    Implemented item table to html

--- index.js
+++ index.js
@@ -11,25 +11,45 @@ async function getInvItems() {
     let gameInvJson = await gameInvResponse.json();
 
     let itemArray = [];
-    Object.values(gameInvJson['rgDescriptions']).forEach(element => {
-        let item = {
-            icon_url: element['icon_url'],
-            market_name: element['market_name'],
-            colour: typeof element['tags'][2]['color'] !== 'undefined' ? element['tags'][2]['color'] : 'bfbfbf' 
-        }
+    Object.values(gameInvJson['rgInventory']).forEach(element => {
+        let itemID = element['classid'];
+        if (typeof itemArray[itemID] === 'undefined') {
+            itemArray[itemID] = {
+                item_id: itemID,
+                amount: 1
+            }
 
-        itemArray.push(item);
+            Object.values(gameInvJson['rgDescriptions']).forEach(element => {
+                if (element['classid'] == itemID) {
+                    itemArray[itemID].market_name = element['market_name'];
+                    itemArray[itemID].colour = typeof element['tags'][2]['color'] !== 'undefined' ? element['tags'][2]['color'] : 'bfbfbf' ;
+                }
+            });
+        }
+        else
+        {
+            itemArray[itemID].amount++;
+        }
     });
 
-    //console.log(itemArray);
+    
     return itemArray;
 }
 
-
 //create html page
 function createHtml(inputPath, outputPath, itemArray) {
-    let $ = cheerio.load(fs.readFileSync(inputPath));
-    $('#item-info').html('testing 123');
+    let $ = cheerio.load(fs.readFileSync(inputPath), {
+        xmlMode: true,
+        decodeEntities: false
+    });
+    let tableContent = '';
+
+    for (var key in itemArray) {
+        tableContent += '<tr>' + itemArray[key].market_name + '</tr>';
+    }
+
+    console.log(tableContent);
+    $('#item-table').text(tableContent.toString());
 
     fs.writeFile(outputPath, $.root().html(), {'encoding': 'utf-8'}, function (err) {
         if (err) {
@@ -38,10 +58,10 @@ function createHtml(inputPath, outputPath, itemArray) {
     });
 }
 
-function main() {
+async function main() {
     const HTML_FILE_PATH_IN = path.join(__dirname, 'template.html');
     const HTML_FILE_PATH_OUT = path.join(__dirname, 'index.html');
-    let itemArray = getInvItems();
+    let itemArray = await getInvItems();
     createHtml(HTML_FILE_PATH_IN, HTML_FILE_PATH_OUT, itemArray);
 }
 
